4bdc5405403d2223b11ea7ca0b44bc66534336b5
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-05-30 01:05:05 +0800

    feat: add route

--- urlShortener/routes/modules/home.js
+++ urlShortener/routes/modules/home.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const shortener = require('../../public/javascript/shortener')
 const { check, validationResult } = require('express-validator')
+const shortUrl = require('../../models/shortUrl')
 
 router.get('/', (req, res) => {
   res.render('index')
@@ -11,11 +12,9 @@ router.post('/', [
 ], (req, res) => {
   const fullUrl = req.body.url
   if (!fullUrl) {
-    req.flash('info', 'Please check your url')
-    return res.redirect('/')
+    res.redirect('index')
   }
   const errors = validationResult(req)
-  console.log(errors)
   if (!errors.isEmpty()) {
     return res.status(422).render('index', {
       errors: errors.array(),
@@ -23,9 +22,22 @@ router.post('/', [
     })
   }
 
-
-  res.render('index')
-
+  shortUrl.find({ full: fullUrl })
+    .lean()
+    .then(url => {
+      if (url) {
+        return res.render('index', { fullUrl, short })
+      }
+      const short = shortener()
+      shortUrl.create({
+        full: fullUrl, short
+      })
+      return res.render('index', { fullUrl, short })
+        .catch(error => {
+          console.log(error)
+          res.render('index', { error: error.message })
+        })
+    })
 })
 
 module.exports = router
