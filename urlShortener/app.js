6cf3633e281bc2d5787f3e538503a94181cd00fe
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-05-29 14:48:47 +0800

    feat: add basic views

--- urlShortener/app.js
+++ urlShortener/app.js
@@ -2,9 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
 
+require('./config/mongoose')
+
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+// setting static files
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.use(routes)
 
 
