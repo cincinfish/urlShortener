4cfcffeeb783bbd99104cc9195f39a6423a75798
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-06-02 18:15:44 +0800

    fix: check random chars

--- urlShortener/public/javascript/shortener.js
+++ urlShortener/public/javascript/shortener.js
@@ -5,17 +5,17 @@ async function shortener() {
   let newUrl = ''
   for (let i = 0; i < 5; i++) {
     newUrl += chars[Math.floor(Math.random() * chars.length)]
-    // await shortUrl.findOne({ short: newUrl })
-    //   .lean()
-    //   .then(url => {
-    //     if (url) {
-    //       i = 0
-    //       newUrl = ''
-    //     }
-    //   })
-    //   .catch(error => {
-    //     console.log(error)
-    //   })
+    await shortUrl.findOne({ short: newUrl })
+      .lean()
+      .then(url => {
+        if (url) {
+          i = 0
+          newUrl = ''
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
   return newUrl
 }
